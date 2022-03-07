bfbff7cc66daedbb5477d62ebe6d9075f578d08c
Aviad_Gilboa <35407628+Avgilboa@users.noreply.github.com>
2022-03-07 16:32:59 +0200

    Add files via upload

--- mat.cpp
+++ mat.cpp
@@ -8,7 +8,7 @@ namespace ariel{
         static const char min_sign = char(33);
         static const char max_sign = char(127);
         if(c==d) {throw std::runtime_error ("the char are the same");}
-        if(c <33 || c> 126 || d<33 || d>126){throw std:: runtime_error ("this bad bag!");}
+        if(c <33 || c> 126 || d<33 || d>126){throw std:: runtime_error ("this char is not ascii!");}
         if(a%2==0 || b%2==0){throw std::runtime_error ("Mat size is always odd") ;}
         if(a<1 || b<1){throw std::runtime_error ("error input negative numbers");}
         return "this function works";
